ea42cd60e8e238f6380bb45bd03f6472688e0d9a
Wojciech Szymański <141330+wszymanski@users.noreply.github.com>
2022-04-27 22:30:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <img src="image.jpg">
+    <?php echo '<p>Hello World</p>'; ?> 
+  </body>
 </html>
